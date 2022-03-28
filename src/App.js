70e4159e2c8d9eeca73bbd0af13ea10aa7f3d96e
Creative Tutorials <68476321+creative-tutorials@users.noreply.github.com>
2022-03-28 17:17:59 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,150 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import AirbnbHome from './AirbnbHome';
+import airbnb_logo from './Airbnb logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
 
-function App() {
+function App() {  
+const [hide, sethide] = useState(true);
+const [show, setshow] = useState(true);
+const validateForm = (event) => {
+  event.preventDefault();
+  const input1 = document.querySelector("#input1");
+  const input2 = document.querySelector("#input2");
+  const status = document.querySelector(".status");
+
+  let validation = "Please fill-in the above fields";
+  let emailVal = "Email validation error must inclued @ .com TryAgain!";
+  let passVal = "Passowrd char must not be less than 8";
+  if(!input1.value) {
+    console.warn("validation error");
+    status.classList.add("active");
+    status.innerHTML = `${validation}`;
+  }else{
+    emailValidate();
+  }
+  function emailValidate() { 
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if(!input1.value.match(pattern)){
+      console.warn("pattern failed")
+      status.classList.add("active");
+      status.innerHTML = `${emailVal}`;
+    }else{
+      console.log("input1 validated");
+      status.classList.remove("active");
+      input1.classList.add("valid");
+    }
+   }
+   if(!input2.value){
+      console.warn("validation error");
+      status.classList.add("active");
+      status.innerHTML = `${validation}`;
+   }else{
+     passwordValidate();
+   }
+   function passwordValidate() {
+     if(input2.value.length < 8){
+       console.warn("Passowrd char must not be less than 8");
+       status.classList.add("active");
+      status.innerHTML = `${passVal}`;
+     }else{
+      console.log("input2 validated");
+      status.classList.remove("active");
+      input2.classList.add("valid");
+     }
+   }
+   if(input1.classList.contains("valid") && input2.classList.contains("valid")){
+     console.log('submitted');
+     setTimeout(() => {
+      //  window.location.href = 'AirbnbHome';
+      let container = document.querySelector(".container");
+      let wrapper = document.querySelector(".wrapper");
+      sethide(!hide + wrapper.classList.add("hide"));
+      setshow(!show + container.classList.add("show"));
+
+       
+     }, 1000);
+   }
+  }
+  const handleMail = () => {
+    const input1 = document.querySelector("#input1");
+    const status = document.querySelector(".status");
+    let validation = "Please fill-in the above fields";
+    let emailVal = "Email validation error must inclued @ .com TryAgain!";
+  if(!input1.value) {
+    console.warn("validation error");
+    status.classList.add("active");
+    status.innerHTML = `${validation}`;
+  }else{
+    emailValidate();
+  }
+  function emailValidate() { 
+    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if(!input1.value.match(pattern)){
+      console.warn("pattern failed")
+      status.classList.add("active");
+      status.innerHTML = `${emailVal}`;
+    }else{
+      console.log("input1 validated");
+      status.classList.remove("active");
+    }
+   }
+  }
+  const handlePassword  = () => {
+    const input2 = document.querySelector("#input2");
+    const status = document.querySelector(".status");
+  
+    let validation = "Please fill-in the above fields";
+    let passVal = "Passowrd char must not be less than 8";
+     if(!input2.value){
+        console.warn("validation error");
+        status.classList.add("active");
+        status.innerHTML = `${validation}`;
+     }else{
+       passwordValidate();
+     }
+     function passwordValidate() {
+       if(input2.value.length < 8){
+         console.warn("Passowrd char must not be less than 8");
+         status.classList.add("active");
+        status.innerHTML = `${passVal}`;
+       }else{
+        console.log("input2 validated");
+        status.classList.remove("active");
+       }
+     }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AirbnbHome />
+      <div className="wrapper">
+        <div className="content">
+          <div className="logo">
+            <img src={airbnb_logo} alt="" />
+          </div>
+          <div className="c1">
+            <span>Become an member of Airbnb today</span>
+          </div>
+          <div className="form">
+            <div className="status"></div>
+            <form action="#" onSubmit={validateForm}>
+              <div className="eInput">
+              <FontAwesomeIcon className='icon' icon={faUser} />
+                <input id='input1' type="text" placeholder='Enter Email Address' onKeyUp={handleMail}/>
+              </div>
+              <div className="eInput">
+              <FontAwesomeIcon className='icon' icon={faLock} />
+                <input id='input2' type="password" placeholder='Enter Password' onKeyUp={handlePassword}/>
+              </div>
+              <div className="submit">
+                <button type="submit">Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
